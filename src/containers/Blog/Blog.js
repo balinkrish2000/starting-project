f1ae304e5cb5bac5e7ffb4da4eebcf070be41d1b
balinkrish2000 <balinkrish2000@gmail.com>
2021-06-26 21:41:47 +0530

    router implementation

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts'
+import {Route, NavLink }from 'react-router-dom'
+import NewPost from './NewPost/NewPost'
 
 import './Blog.css';
 
@@ -10,12 +12,29 @@ class Blog extends Component {
                 <header className="Blog">
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><NavLink 
+                                to="/" 
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={
+                                    {
+                                        color: '#fa93f',
+                                        textDecoration: 'underline'
+                                    }
+                                }>Home</NavLink></li>
+                            <li><NavLink to={
+                                {
+                                    pathname: '/new-post',
+                                    hash: '#submit',
+                                    search: '?quick-sumit=true'
+                                }
+                            }>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+                {/*<Route path="/"  exact render={() => <Posts/>}/>*/}
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost}/>
             </div>
         );
     }
